a0ac3071db567b6c7f6cd502bc187d9394f9354e
Soumya <soumyabb2015@gmail.com>
2021-10-18 15:58:29 +0530

    working on dashborad

--- unit-5-eval-1/src/pages/Dashboard.jsx
+++ unit-5-eval-1/src/pages/Dashboard.jsx
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router";
 
 export default function Dashboard() {
   const { isAuth } = useSelector((state) => state.auth);
-  return !isAuth ? <Redirect to="/login" /> : <div>Dashboard</div>;
+  const [addMeet, setAddMeet] = useState(false);
+  const [meetTitle, setMeetTitle] = useState("");
+  const [meetDescription, setMeetDescription] = useState("");
+  const [meetDate, setMeetDate] = useState("");
+  const [meetTime, setMeetTime] = useState("");
+
+  const handleNewMeet = () => {};
+
+  return !isAuth ? (
+    <Redirect to="/login" />
+  ) : (
+    <div>
+      <div>
+        {addMeet && (
+          <form onSubmit={handleNewMeet}>
+            <input
+              type="text"
+              placeholder="title"
+              onChange={(e) => setMeetTitle(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              placeholder="description"
+              onChange={(e) => setMeetDescription(e.target.value)}
+            />
+            <br />
+            <input
+              type="date"
+              placeholder="date"
+              onChange={(e) => setMeetDate(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              placeholder="time"
+              onChange={(e) => setMeetTime(e.target.value)}
+            />
+            <br />
+            <button onClick={() => setAddMeet(false)}>Cancel</button>
+          </form>
+        )}
+      </div>
+      <button onClick={() => setAddMeet(true)}>Add Meet</button>
+    </div>
+  );
 }
+// title, description, optionally image, date and time of occurrence, rating
